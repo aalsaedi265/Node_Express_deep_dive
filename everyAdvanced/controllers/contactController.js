5b414b49c3ba25364aabe54fe4687dfa117e14ac
aalsaedi265 <aalsaedi265@gmail.com>
2023-04-02 10:41:14 -0500

    starting error handeling

--- everyAdvanced/controllers/contactController.js
+++ everyAdvanced/controllers/contactController.js
@@ -2,10 +2,17 @@
 const getContact=  (req, res) => {
     res.status(200).json( {"GER":"Absaloute Zero"})
 }
+
 const createContact = (req, res) => {
     console.log("the request body is ", req.body)
+    const {name, phone, occupation} = req.body
+    if(!name || !phone || !occupation){
+        res.status(400).json
+        throw new Error("All fields are required")
+    }
     res.status(201).json( {"GER":"Life Creation"})
 }
+
 const deleteContact =  (req, res) => {
     res.status(200).json( {"Deadly Queen":`Bite The Dust ${req.params.id}`})
 }
--- everyAdvanced/server.js
+++ everyAdvanced/server.js
@@ -6,6 +6,7 @@ const app = express();
 //prevent already runining errror, this will find which ever is etmpy and use it
 const port = process.env.PORT || 5001
 //use is a middleware
+app.use(express.json() ) 
 app.use("/api/contacts", require("./routes/contactRoutes.js"))
 
 app.listen(port, ()=>{
